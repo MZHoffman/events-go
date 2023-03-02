574ffc1c1553e850b194d0637ee7ba74b568e8ca
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-03-02 14:34:09 +0000

    Creating routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './pages/RootLayout'
+import HomePage from './pages/HomePage'
+import EditEventPage from './pages/EditEventPage'
+import EventDetailPage from './pages/EventDetailPage'
+import EventsPage from './pages/EventsPage'
+import NewEventPage from './pages/NewEventPage'
+import ErrorPage from './pages/ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '', element: <HomePage /> },
+      { path: 'events', element: <EventsPage /> },
+      { path: 'events/:eventId', element: <EventDetailPage /> },
+      { path: 'events/:eventId/edit', element: <EditEventPage /> },
+      { path: 'new-event', element: <NewEventPage /> },
+    ],
+  },
+])
 
 const App = () => {
-  return <div>App</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
