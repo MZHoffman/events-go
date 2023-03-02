617819a78e26601dc9d058e9bcc257cc36a0e02c
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-03-02 20:12:49 +0000

    Fetching data with react-router-dom loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,18 @@ const router = createBrowserRouter([
         path: 'events',
         element: <EventsRootLayout />,
         children: [
-          { path: '', element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch('http://localhost:8080/events')
+              if (!response.ok) {
+              } else {
+                const resData = await response.json()
+                return resData.events
+              }
+            },
+          },
           { path: ':eventId', element: <EventDetailPage /> },
           { path: ':eventId/edit', element: <EditEventPage /> },
           { path: 'new', element: <NewEventPage /> },
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,25 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 
-const EVENTS = [
-  { id: 'e1', name: 'Event 1' },
-  { id: 'e2', name: 'Event 2' },
-  { id: 'e3', name: 'Event 3' },
-  { id: 'e4', name: 'Event 4' },
-  { id: 'e5', name: 'Event 5' },
-]
+import EventsList from '../components/EventsList'
 
 const EventsPage = () => {
+  const events = useLoaderData()
   return (
     <>
-      <h1>Events</h1>
-      <ul>
-        {EVENTS.map((e) => (
-          <li key={e.id}>
-            <Link to={e.id}>{e.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   )
 }
