bca1b940866c2eaa6067e2d828511f26b3b137f5
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-03-02 15:16:51 +0000

    Addin dummy events

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const EVENTS = [
+  { id: 'e1', name: 'Event 1' },
+  { id: 'e2', name: 'Event 2' },
+  { id: 'e3', name: 'Event 3' },
+  { id: 'e4', name: 'Event 4' },
+  { id: 'e5', name: 'Event 5' },
+]
 
 const EventsPage = () => {
-  return <div>EventsPage</div>
+  return (
+    <>
+      <h1>Events</h1>
+      <ul>
+        {EVENTS.map((e) => (
+          <li key={e.id}>
+            <Link to={e.id}>{e.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default EventsPage
