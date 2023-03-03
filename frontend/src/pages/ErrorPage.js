642f7383b32db0338fa54ca5517ad16390aa0719
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-03-03 14:48:28 +0000

    providing more info in error

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -1,9 +1,23 @@
+import { useRouteError } from 'react-router-dom'
 import PageContent from '../components/PageContent'
 
 const ErrorPage = () => {
+  const error = useRouteError()
+
+  let title = 'An error occured!'
+  let message = 'Something went wrong!'
+
+  if (error.status === 500) {
+    message = error.data.message
+  }
+  if (error.status === 404) {
+    title = 'Not found!'
+    message = 'Could not find resource or page!'
+  }
+
   return (
-    <PageContent title='An error occured'>
-      <p>Something went wrong</p>
+    <PageContent title={title}>
+      <p>{message}</p>
     </PageContent>
   )
 }
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, json } from 'react-router-dom'
 
 import EventsList from '../components/EventsList'
 
@@ -16,7 +16,10 @@ export default EventsPage
 export const eventsLoader = async () => {
   const response = await fetch('http://localhost:8080/events')
   if (!response.ok) {
-    throw { message: 'Could not fetch events!' }
+    // throw new Response(
+    //   JSON.stringify({ message: 'Counld not fetch events!' }),
+    //   { status: 500 }
+    throw json({ message: 'Could not fetch events.' }, { status: 500 })
   } else {
     return response
   }
