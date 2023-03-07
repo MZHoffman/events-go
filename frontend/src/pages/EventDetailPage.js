747bfd50c150c9e5aa98559e76abec98e4e75c45
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-03-07 17:07:29 +0000

    Deleting event

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useRouteLoaderData, json } from 'react-router-dom'
+import { useRouteLoaderData, json, redirect } from 'react-router-dom'
 
 import EventItem from '../components/EventItem'
 
@@ -21,5 +21,17 @@ export const eventDetailLoader = async ({ reqest, params }) => {
     )
   } else {
     return response
+  }
+}
+
+export const deleteItemAction = async ({ params, request }) => {
+  const id = params.eventId
+  const response = await fetch('http://localhost:8080/events/' + id, {
+    method: request.method,
+  })
+  if (!response.ok) {
+    throw json({ message: 'Could not delete  event!' }, { status: 500 })
+  } else {
+    return redirect('/events')
   }
 }
