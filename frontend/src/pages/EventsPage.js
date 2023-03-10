c069b356a71e2e380bb7bdb162135a4c37d77200
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-03-10 13:50:23 +0000

    Displaying page before events load using defer

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,23 +1,34 @@
-import { useLoaderData, json } from 'react-router-dom'
+import { Suspense } from 'react'
+import { useLoaderData, json, defer, Await } from 'react-router-dom'
 
 import EventsList from '../components/EventsList'
 
 const EventsPage = () => {
-  const data = useLoaderData()
+  const { events } = useLoaderData()
+
   return (
-    <>
-      <EventsList events={data.events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   )
 }
 
 export default EventsPage
 
-export const eventsLoader = async () => {
+const loadEvents = async () => {
   const response = await fetch('http://localhost:8080/events')
   if (!response.ok) {
     throw json({ message: 'Could not fetch events.' }, { status: 500 })
   } else {
-    return response
+    const resData = await response.json()
+    return resData.events
   }
+}
+
+export const eventsLoader = () => {
+  return defer({
+    events: loadEvents(),
+  })
 }
