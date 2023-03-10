ea978b4db751b945134ee7ba7893fefd789bbe0d
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-03-10 15:01:54 +0000

    Welcome message in teh home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <PageContent title='Welcome!'>
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  )
 }
 
 export default HomePage
